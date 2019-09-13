e45b2540072eb5386fde9b5b66d871911fab1708
Ivank <ivank@192.168.0.5>
2019-09-12 22:37:08 -0500

    cree Perfil

---
+++ front/src/components/Perfil.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+
+export class Perfil extends Component {
+    render() {
+        return (
+            <Container> 
+                <Col md={4}>
+
+                </Col>
+                <Col md={8}></Col>
+            </Container>
+        )
+    }
+}
+
+export default Perfil
