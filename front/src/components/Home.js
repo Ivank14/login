fde9fdf9d6617f18c53ce708b5256571e3bfcdeb
Nartakar <kamilongis@hotmail.com>
2019-09-13 01:48:07 -0500

    home

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,11 +1,26 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react';
+import { ParallaxProvider } from 'react-scroll-parallax';
+import { Parallax, withController } from 'react-scroll-parallax';
+ 
+class Image extends Component {
+    handleLoad = () => {
+        // updates cached values after image dimensions have loaded
+        this.props.parallaxController.update();
+    };
+ 
+    render() {
+        return <img src="../assets/galaxy.jpg" onLoad={this.handleLoad} />;
+    }
+}
 export class Home extends Component {
     render() {
         return (
-           <div>
-
-           </div> 
+            <ParallaxProvider>
+                <Parallax className="custom-class" y={[-20, 20]} tagOuter="figure">
+                    <Image />
+                    <img src="../assets/galaxy.jpg"/>
+                </Parallax>
+            </ParallaxProvider>
         )
     }
 }
