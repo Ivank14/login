3da73889d7203d2958e43b8cd62b502a372c44c4
Nartakar <kamilongis@hotmail.com>
2019-09-13 01:05:09 -0500

    home

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Perfil from './components/Perfil'
 
 import Loginfunction from './components/Login'
+import Home from './components/Home'
 import { Route, Router } from 'react-router-dom'
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
@@ -29,6 +30,7 @@ function App() {
       <Router history={history}>
         <Route path="/" exact component={Loginfunction} />
         <Route path="/perfil" component={Perfil} />
+        <Route path="/home" component={Home} />
       </Router>
     </ApolloProvider>
   );
