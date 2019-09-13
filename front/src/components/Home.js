cc8742a08c6e8a823424bcd1dfeac909821dad0c
Nartakar <kamilongis@hotmail.com>
2019-09-13 01:05:58 -0500

    otro

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-import { Parallax, Background } from 'react-parallax';
 
 export class Home extends Component {
     render() {
         return (
-            
+           <div>
+
+           </div> 
         )
     }
 }
